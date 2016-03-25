da43b1dae2fddcb41c783add35eceea5b320a688
Mike Walsh <mikewalsh2@gmail.com>
2016-03-25 21:42:45 +0000

    Adding local Karma/Jasmine dependencies.

---
+++ public/js/test/controllers.js
@@ -0,0 +1,14 @@
+describe('controllers', function() {
+    beforeEach(module('MongoUI'));
+
+    beforeEach();
+
+    describe('mongoController', function() {
+        it('should broadcast a schema load when clicked', function (_$controller_) {
+            var scope = {},
+                ctrl = _$controller_('mongoController', {$scope: scope});
+
+
+        });
+    });
+});
